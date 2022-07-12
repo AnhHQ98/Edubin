4153beff78f02fa12d77e0941a973eb2617ab564
AnhHQ98 <99629266+AnhHQ98@users.noreply.github.com>
2022-07-12 16:08:13 +0700

    fix

--- script2.js
+++ script2.js
@@ -1,38 +1,71 @@
-// Form
-function Validator(options){
-    var formElement = document.querySelector(options.form)
-    if (formElement){
-        options.rules.forEach(function(rule){
-            var inputElement = formElement.querySelector(rule.selector);
-            var errorElement = inputElement.parentElement.querySelector('.form-message');
-            if (inputElement) {
-                inputElement.onblur = function(){
-                    var errorMessage = rule.test(inputElement.value);
-                    if (errorMessage) {
-                        errorElement.innerText = errorMessage;
-                        inputElement.parentElement.classList.add('invalid');
-                    } else {
-                        errorElement.innerText = '';
-                        inputElement.parentElement.classList.remove('invalid');
-                    }
-                }
+
+// cach 1
+// const regexName = /^[a-z ,.'-]+$/i;
+// const firstName = document.getElementById("fname")
+// const errFirstName = document.getElementById("1")
+// firstName.addEventListener("blur", () => {
+//     if(firstName.value === "") {
+//         errFirstName.style.display = "block"
+//         firstName.style.borderColor = "red"
+//     }
+//    else {
+//     if(!regexName.test(firstName.value)) {
+//         errFirstName.style.display = "block"
+//         firstName.style.borderColor = "red"
+//     }
+//     else {
+//         errFirstName.style.display = "none"
+//         firstName.style.borderColor = "#ccc"
+//     }
+//    }
+// })
+
+
+// cach 2
+// const regexName = /^[a-z ,.'-]+$/i;
+// const firstName = document.getElementById("fname")
+// const lastName = document.getElementById("lname")
+// const errFirstName = document.getElementById("1")
+// function validateInput() {
+//     if (firstName.value === ""){
+//         errFirstName.style.display = 'block';
+//     } else {
+//         errFirstName.style.display = 'none';
+//     }
+// }
+// firstName.addEventListener('blur', validateInput)
+// lastName.addEventListener('blur', validateInput)
+
+
+// cach 3
+const allInput = document.querySelectorAll('input');
+const regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+const e_mail = document.getElementById('email') 
+const errMessage = document.querySelectorAll('.form-message')
+
+
+console.log(allInput);
+
+function validateInput(e){
+    e.preventDefault();  
+    for ( let i=0; i <= allInput.length; i++) {
+        if(allInput[i].value === "") {       
+            errMessage[i].innerHTML = "Khong duoc de trong !"
+            allInput[i].style.borderColor = 'red'
+            errMessage[i].style.color = 'red'
+        }
+        else {
+            if (!regexEmail.test(e_mail.value)) {
+                errMessage[3].innerHTML = "Vui long nha dung email"
+                allInput[3].style.borderColor = 'red'
+                errMessage[3].style.color = 'red'
             }
-        });
+           
+        }
     }
 }
-Validator.isRequired = function(selector){
-    return {
-        selector: selector,
-        test: function(value){
-            return value.trim() ? undefined : 'Vui lòng nhập vào đây'
-        }
-    };
-}
-Validator.isEmail = function(selector){
-    return {
-        selector: selector,
-        test: function(){
-            
-        }
-    };
-}
+// email
+const btn = document.getElementById("btnOk")
+btn.addEventListener("click", validateInput)
+
+
