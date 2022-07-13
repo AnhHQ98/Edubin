b2cedf1346b43ac247f6da61e52d43d812ed945b
AnhHQ98 <99629266+AnhHQ98@users.noreply.github.com>
2022-07-13 08:58:45 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,39 +1,3 @@
-// const regexName = /^[a-z ,.'-]+$/i;
-// const firstName = document.getElementById("fname")
-// const errFirstName = document.getElementById("1")
-// firstName.addEventListener("blur", () => {
-//     if(firstName.value === "") {
-//         errFirstName.style.display = "block"
-//         firstName.style.borderColor = "red"
-//     }
-//    else {
-//     if(!regexName.test(firstName.value)) {
-//         errFirstName.style.display = "block"
-//         firstName.style.borderColor = "red"
-//     }
-//     else {
-//         errFirstName.style.display = "none"
-//         firstName.style.borderColor = "#ccc"
-//     }
-//    }
-// })
-
-
-// const regexName = /^[a-z ,.'-]+$/i;
-// const firstName = document.getElementById("fname")
-// const lastName = document.getElementById("lname")
-// const errFirstName = document.getElementById("1")
-// function validateInput() {
-//     if (firstName.value === ""){
-//         errFirstName.style.display = 'block';
-//     } else {
-//         errFirstName.style.display = 'none';
-//     }
-// }
-// firstName.addEventListener('blur', validateInput)
-// lastName.addEventListener('blur', validateInput)
-
-
 const allInput = document.querySelectorAll('input');
 const regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
 const e_mail = document.getElementById('email')
